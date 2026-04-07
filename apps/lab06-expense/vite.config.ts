67222ee16de612c3b4173f5592eada778f537c41
tnapatsakorn <napatsakorn.ch@kkumail.com>
2026-04-07 16:17:49 +0700

    fix: add base path for github pages

--- apps/lab06-expense/vite.config.ts
+++ apps/lab06-expense/vite.config.ts
@@ -7,15 +7,19 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mobileweb/lab06-expense/', // 🔥 เพิ่มบรรทัดนี้
+
   plugins: [
     vue(),
     legacy()
   ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   test: {
     globals: true,
     environment: 'jsdom'
